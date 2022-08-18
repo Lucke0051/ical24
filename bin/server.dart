5b9d48c17b9b16c27f6c1ae8144e2b86b3f673f6
Lukas <30857678+Lucke0051@users.noreply.github.com>
2022-08-18 09:06:41 +0200

    test

--- bin/server.dart
+++ bin/server.dart
@@ -63,7 +63,7 @@ Future<Response> _getCalendarHandler(Request request) async {
 
   final ICalendar calendar = ICalendar(company: "skola24", product: "skola/schema", lang: "SV", refreshInterval: const Duration(hours: 4));
   for (final skola24.Lesson lesson in allLessons) {
-    print("${lesson.name} ${lesson.start.toIso8601String()}");
+    print("${lesson.name} local: ${lesson.start.toLocal().toIso8601String()} utc: ${lesson.start.toUtc().toIso8601String()}");
     final IEvent event = IEvent(
       uid: lesson.guid,
       start: lesson.start,
